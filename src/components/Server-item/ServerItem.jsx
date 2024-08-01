2cda2bce20b6c79d82712c70eebac627e76caac4
Artem Karpuk <artemkovel2007@gmail.com>
2024-08-02 00:05:08 +0200

    Add new function button delete server which can delete server and file
     Changes to be committed:
    	modified:   src/components/Server-item/ServerItem.jsx

--- src/components/Server-item/ServerItem.jsx
+++ src/components/Server-item/ServerItem.jsx
@@ -1,12 +1,19 @@
+import axios from "axios";
 
 export default function ServerItem({ server }) {
 
+    function deleteServer() {
+        axios.delete(`http://localhost:3333/api/server/${server._id}`)
+        window.location.reload();
+    }
 
     return (
         <div id="serverItem">
             <h1>{server.name}</h1>
             <p>version: {server.version}</p>
             <p>core: {server.core}</p>
+
+            <button onClick={deleteServer}>Delete</button>
         </div>
     );
 };
